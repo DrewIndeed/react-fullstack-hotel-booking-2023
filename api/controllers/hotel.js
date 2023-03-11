15b1f44fe7c42ee6ab05497f97a123adfb77ee9d
an.le6 <anle.drewindeed@gmail.com>
2023-03-11 09:46:24 +0700

    6. feat: hotel controllers

---
+++ api/controllers/hotel.js
@@ -0,0 +1,58 @@
+import Hotel from "../models/Hotel.js";
+
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    console.log(error); // DEV
+    next(error);
+  }
+};
+
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true } // NOTES: return new record as response
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    console.log(error); // DEV
+    next(error);
+  }
+};
+
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json(`Hotel deleted - ID: ${req.params.id}`);
+  } catch (error) {
+    console.log(error); // DEV
+    next(error);
+  }
+};
+
+export const getHotel = async (req, res, next) => {
+  try {
+    const recordHotel = await Hotel.findById(req.params.id);
+    res.status(200).json(recordHotel);
+  } catch (error) {
+    console.log(error); // DEV
+    next(error);
+  }
+};
+
+export const getHotels = async (req, res, next) => {
+  try {
+    const allHotels = await Hotel.find();
+    res.status(200).json(allHotels);
+  } catch (error) {
+    console.log(error); // DEV
+    next(error);
+  }
+};
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,69 +1,23 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (error) {
-    console.log(error); // DEV
-    res.status(500).json(error);
-  }
-});
+router.post("/", createHotel);
 // UPDATE
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true } // NOTES: return new record as response
-    );
-    res.status(200).json(updatedHotel);
-  } catch (error) {
-    console.log(error); // DEV
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", updateHotel);
 // DELETE
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json(`Hotel deleted - ID: ${req.params.id}`);
-  } catch (error) {
-    console.log(error); // DEV
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", deleteHotel);
 // GET
-router.get("/:id", async (req, res) => {
-  try {
-    const recordHotel = await Hotel.findById(req.params.id);
-    res.status(200).json(recordHotel);
-  } catch (error) {
-    console.log(error); // DEV
-    res.status(500).json(error);
-  }
-});
+router.get("/:id", getHotel);
 // GET ALL
-router.get("/", async (req, res, next) => {
-  // fake err config
-  const failed = true;
-  if (failed) return next(createError(401, "You are not authenticated!"));
-
-  try {
-    const allHotels = await Hotel.findById("asdjfkhasdasdfasdfasdfasdfkjf");
-    res.status(200).json(allHotels);
-  } catch (error) {
-    console.log(error); // DEV
-    next(error);
-  }
-});
+router.get("/", getHotels);
 
 export default router;
